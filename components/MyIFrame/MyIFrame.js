4e283dd130de2cc6efd6c3af793574b219ec5b8a
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-29 15:46:08 -0300

    I init components folder.

---
+++ components/MyIFrame/MyIFrame.js
@@ -0,0 +1,12 @@
+const MyIFrame = () => {
+  return (
+    <iframe
+      src="/api/iframe"
+      height="200"
+      width="300"
+      title="Iframe Example"
+    ></iframe>
+  );
+};
+
+export default MyIFrame;
---
+++ components/MyIFrame/index.js
@@ -0,0 +1,3 @@
+import MyIFrame from "./MyIFrame";
+
+export default MyIFrame;
