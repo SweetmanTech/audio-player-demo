e9b1d518087725a69fd626c1f6390ac4921a20b1
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-30 15:55:42 -0300

    WIP

--- components/MyIFrame/MyIFrame.js
+++ components/MyIFrame/MyIFrame.js
@@ -1,7 +1,7 @@
 const MyIFrame = () => {
   return (
     <iframe
-      src="/iframe"
+      src="https://minting-studio-kaf8ekx61-decent-webapp.vercel.app/420/Editions/0x91a738dd4B3344137E1fA9651746Dba2bcC05073/iframe"
       height="500"
       width="500"
       title="Iframe Example"
--- pages/iframe.js
+++ pages/iframe.js
@@ -3,8 +3,71 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 const Iframe = () => {
   return (
     <div>
-      <ConnectButton />
-      <h1>HELLO WORLD</h1>
+      <h1>Step 1: Follow Me On Lens</h1>
+      <button>LENS FOLLOW BUTTON</button>
+      <h1>Step 2: Collect Pin</h1>
+      <p>Current Price: 1.5 ETH</p>
+      <ConnectButton.Custom>
+        {({
+          account,
+          chain,
+          openAccountModal,
+          openChainModal,
+          openConnectModal,
+          authenticationStatus,
+          mounted,
+        }) => {
+          // Note: If your app doesn't use authentication, you
+          // can remove all 'authenticationStatus' checks
+          const ready = mounted && authenticationStatus !== "loading";
+          const connected =
+            ready &&
+            account &&
+            chain &&
+            (!authenticationStatus || authenticationStatus === "authenticated");
+
+          return (
+            <div
+              {...(!ready && {
+                "aria-hidden": true,
+                style: {
+                  opacity: 0,
+                  pointerEvents: "none",
+                  userSelect: "none",
+                },
+              })}
+            >
+              {(() => {
+                if (!connected) {
+                  return (
+                    <button onClick={openConnectModal} type="button">
+                      Connect Wallet
+                    </button>
+                  );
+                }
+
+                if (chain.unsupported) {
+                  return (
+                    <button onClick={openChainModal} type="button">
+                      Wrong network
+                    </button>
+                  );
+                }
+
+                return (
+                  <div style={{ display: "flex", gap: 12 }}>
+                    <button onClick={openAccountModal} type="button">
+                      BUY NOW
+                    </button>
+                  </div>
+                );
+              })()}
+            </div>
+          );
+        }}
+      </ConnectButton.Custom>
+      <h1>Step 3: Enter Vault</h1>
+      <button>Enter Vault</button>
     </div>
   );
 };
